b16c2c361599d33367d82a8d2f15f5e221fef07f
Julio Marcon <47875423+juliomarcon@users.noreply.github.com>
2022-09-23 21:50:43 -0300

    Add files via upload

--- Aula 2/Conta.cs
+++ Aula 2/Conta.cs
@@ -1,13 +1,29 @@
 class Conta{
+    //atributos estáticos
+    public static int quantContas = 0;
+
+    //atributos
     public string numero;
     public string titular;
-    public decimal saldo;
+    List<Transacao> movimentacao = new List<Transacao>();
+
+    public decimal saldo{
+        get{
+            decimal valorFinal = 0;
+            foreach(Transacao transacao in movimentacao){
+                valorFinal += transacao.valor;
+            }
+            return valorFinal;
+        }
+    }
 
     //construtor
     public Conta(string num, string tit, decimal sal){
         this.numero = num;
         this.titular = tit;
-        this.saldo = sal;
+        this.depositar(sal, DateTime.Now, "Deposito Inicial");
+
+        quantContas ++;
     }
     //métodos
     public string mostrarDados(){
@@ -20,5 +36,14 @@ class Conta{
         return texto;
     }
 
+    public void depositar(decimal val, DateTime dat, string mot){
+        Transacao deposito = new(val,dat,mot);
+        this.movimentacao.Add(deposito);
+    }
+
+    public void retirar(decimal val, DateTime dat, string mot){
+        Transacao saque = new Transacao (-val, dat, mot);
+        this.movimentacao.Add(saque);
+    }
 
 }
--- Aula 2/Program.cs
+++ Aula 2/Program.cs
@@ -1,11 +1,17 @@
 ﻿Console.Clear();
 
 Conta contaZeColmeia = new Conta("123", "Zé Colméia", 500);
+contaZeColmeia.retirar(145,DateTime.Now,"Pagamento da conta de luz");
 Console.WriteLine( contaZeColmeia. mostrarDados() );
 Console.WriteLine();
 Console.ReadKey();
 
 Conta contaCatatau = new Conta("456", "Catatau", 340);
+contaCatatau.retirar(145, DateTime.Now, "Camisera do curso");
 Console.WriteLine( contaCatatau.mostrarDados() );
 Console.WriteLine();
 Console.ReadKey();
+
+Console.WriteLine("Contas criadas: " + Conta.quantContas);
+Console.ReadKey();
+
